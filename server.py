4223712828977a155774e53ea21e76f353647aec
Rob Johansen <r.johansen@edge47.com>
2019-11-13 18:27:45 +0000

    search imdb

--- server.py
+++ server.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python
 
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
+from imdb import IMDb
+ia = IMDb()
+
+@app.route("/test")
+def imdb():
+    search = request.args.get('search')
+
+    results = ia.search_movie(search)
+    
+    return render_template('results.html', results=results)
 
 @app.route("/")
 def index():
