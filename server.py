cfe2319c24ca83367d3430cd0a751eec060bbc6d
Rob Johansen <r.johansen@edge47.com>
2020-06-07 13:55:28 +0100

    add favicon

--- server.py
+++ server.py
@@ -43,6 +43,9 @@ def index():
 
     return render_template('index.html')
 
+@app.route('/favicon.ico')
+def favicon():
+    return app.send_static_file('favicon.ico')
 
 if __name__ == "__main__":
     app.run(
