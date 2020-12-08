67be44329224ed3d098668fc207ae7a5acb8234d
Rob Johansen <r.johansen@edge47.com>
2020-12-08 19:04:51 +0000

    added node terms

--- app/models.py
+++ app/models.py
@@ -13,6 +13,15 @@ class Node(db.Model):
     parent_id = db.Column(db.Integer, db.ForeignKey('node.id'))
     timestamp = db.Column(db.DateTime, index=True)
     children = db.relationship("Node", lazy="joined", join_depth=2)
+    terms = db.relationship("NodeTerm", lazy="joined")
 
     def __repr__(self):
         return '<Node {}>'.format(self.name)
+
+class NodeTerm(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    term = db.Column(db.String(64), index=True, unique=True)
+    node_id = db.Column(db.Integer, db.ForeignKey('node.id'))
+
+    def __repr__(self):
+        return '<NodeTerm {} {}>'.format(self.term, self.node_id)
--- tilde.py
+++ tilde.py
@@ -1,10 +1,11 @@
 from app import app, db
-from app.models import User, Node
+from app.models import User, Node, NodeTerm
 
 @app.shell_context_processor
 def make_shell_context():
     return {
         'db': db,
         'User': User,
-        'Node': Node
+        'Node': Node,
+        'NodeTerm': NodeTerm
     }
